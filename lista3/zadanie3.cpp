d775e2adb36573481b0555f1e10683984b661f8b
Aleksander Szpatowski <szpaku1999@gmail.com>
2020-04-01 11:37:50 +0200

    poprawki

--- lista3/zadanie3.cpp
+++ lista3/zadanie3.cpp
@@ -4,7 +4,7 @@
 int main()
 {
     std::clock_t start;
-    unsigned N = 10000; //8600000 zeby wykonywalo sie ok. sekunde
+    unsigned N = 8600000; //8600000 zeby wykonywalo sie ok. sekunde
     start = std::clock();
     for (unsigned n=1;n<=N;++n)
     {
@@ -12,13 +12,14 @@ int main()
         delete [] p;// niszczy zarezerowane miejsce w pamieci
     }
     std::cout<<( std::clock() - start ) / (double) CLOCKS_PER_SEC<<"\n";
+    int zmienna=0;
     start = std::clock();
     for (unsigned n=1;n<=N;++n)
     {
         //nie za bardzo rozumiem o co chodziło dokładnie doktorowi Kozie z porownaniem kosztu "pojedynczego dodawania" (przeczytałem rozdział 4)
         
-        std::list <int> p(n); //ale moze o to
+        zmienna= zmienna+1; //jednak o to
     }
     std::cout<<( std::clock() - start ) / (double) CLOCKS_PER_SEC<<"\n";
-    //z tego wychodzi ze koszt pary new/delete jest 10^4 razy mniejszy od pojedynczego dodawania.
+    //z tego wychodzi ze koszt pary new/delete jest ponad 100 razy większy od pojedynczego dodawania.
 }
